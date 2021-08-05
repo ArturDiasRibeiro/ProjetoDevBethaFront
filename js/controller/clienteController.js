cde46aba4b2bd2bb9c1235c399bd7fde6155eb79
Artur Dias Ribeiro <Arturdias1010@hotmail.com>
2021-08-05 17:24:53 -0300

    Tentei

--- js/controller/clienteController.js
+++ js/controller/clienteController.js
@@ -1,14 +1,27 @@
-angular.module("ProjetoBethaFrontEnd").controller("clienteController", function($scope, clienteService) {
+angular.module("ProjetoBethaFrontEnd").controller("clienteController", function ($scope, clienteService) {
     $scope.clientes = []
 
-    loadClientes = function() {
-        clienteService.findAll().then(function(response){
+    loadClientes = function () {
+        clienteService.findAll().then(function (response) {
             console.log(response)
             $scope.clientes = response.data
-        }).catch(function(error){
+        }).catch(function (error) {
+            alert(error.data.message)
+        })
+    }
+    loadClientes()
+
+    loadCliente = function (getId) {
+        let id = getId
+        
+        clienteService.find(id).then(function(response) {
+            console.log(response)
+
+
+        }).catch(function (error) {
             alert(error.data.message)
         })
     }
 
-    loadClientes()
+    loadCliente()
 })
--- js/service/clienteService.js
+++ js/service/clienteService.js
@@ -1,5 +1,12 @@
 angular.module("ProjetoBethaFrontEnd").service("clienteService", function($http, configValue){
+
     this.findAll = function(){
         return $http.get(configValue.apiUrl+"/clientes")
     }
+
+   
+    this.find = function(){
+       return $http.get(configValue.apiUrl+"/clientes/"+getId)
+   }
+   
 })
