d3a77c684c6f6a7bd6702d48fc6d0eeb4be7891d
ArturDiasRibeiro <arturdias1010@hotmail.com>
2021-08-06 12:01:46 -0300

    Aba de clientes 'quase' pronta

--- js/controller/clienteController.js
+++ js/controller/clienteController.js
@@ -1,14 +1,62 @@
-angular.module("ProjetoBethaFrontEnd").controller("clienteController", function($scope, clienteService) {
+angular.module("ProjetoBethaFrontEnd").controller("clienteController", function ($scope, clienteService) {
+    $scope.app = "Clientes"
     $scope.clientes = []
+    $scope.showClientes = true;
+    $scope.showPutCliente = false;
+    $scope.showNGShowGetCliente = false;
 
-    loadClientes = function() {
-        clienteService.findAll().then(function(response){
+    //Get all
+    loadClientes = function () {
+        clienteService.findAll().then(function (response) {
             console.log(response)
             $scope.clientes = response.data
+        }).catch(function (error) {
+            alert(error.data.message)
+        })
+    } 
+    loadClientes()
+
+    //Get one
+    $scope.loadCliente = function (clienteId) {
+        clienteService.findOneById(clienteId).then(function(response){
+            $scope.cliente = response.data
+            console.log(response.data)
+        }).catch(function(error){
+            alert(error.data.message)
+        })
+    } 
+    
+    //put one
+    $scope.putCliente = function(cliente) {
+        clienteService.putCliente(cliente).then(function(response){
+            console.log(response.data)
+            this.loadClientes()      
         }).catch(function(error){
             alert(error.data.message)
         })
     }
 
-    loadClientes()
+    $scope.postCliente = function(cliente) {
+        clienteService.postCliente(cliente).then(function(response){
+            this.loadClientes()
+        }).catch(function(error){
+            alert(error.data.message)
+        })
+    }
+    
+    $scope.showPutCli = function(clienteId) {
+        this.loadCliente(clienteId)
+        $scope.showClientes = false;
+        $scope.showPutCliente = true;
+    }
+
+    $scope.showCli = function() {
+        $scope.showClientes = true;
+        $scope.showPutCliente = false;
+    }
+
+    $scope.showNGShowGetClienteFunction = function(clienteId){
+        this.loadCliente(clienteId)
+        $scope.showNGShowGetCliente= true;
+    }
 })
--- js/service/clienteService.js
+++ js/service/clienteService.js
@@ -1,5 +1,18 @@
 angular.module("ProjetoBethaFrontEnd").service("clienteService", function($http, configValue){
+
     this.findAll = function(){
         return $http.get(configValue.apiUrl+"/clientes")
+    }
+
+    this.findOneById = function(clienteId){
+        return $http.get(configValue.apiUrl+"/clientes/"+clienteId)
+    }
+
+    this.putCliente = function(cliente){
+        return $http.put(configValue.apiUrl+"/clientes/"+cliente.id, cliente)
+    }
+
+    this.postCliente = function(cliente){
+        return $http.post(configValue.apiUrl+"/clientes", cliente)
     }
 })
