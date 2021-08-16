5312998f0fa782e914a958ddd131cce45a06de95
Artur Dias Ribeiro <Arturdias1010@hotmail.com>
2021-08-16 20:51:07 -0300

    algumas mudanças em casa

--- js/controller/clienteController.js
+++ js/controller/clienteController.js
@@ -3,9 +3,10 @@ angular.module("ProjetoBethaFrontEnd").controller("clienteController", function 
     $scope.app = "Área de Clientes"
 
     $scope.clientes = []
- 
+    $scope.clienteEncontrado = false
+  
     //get all Clientes
-    loadClientes = function () {
+    findClientes = function () {
         clienteService.findAll().then(function (response) {
             console.log(response)
             $scope.clientes = response.data
@@ -13,44 +14,34 @@ angular.module("ProjetoBethaFrontEnd").controller("clienteController", function 
             alert(error.data.message)
         })
     }
-    loadClientes()
+    findClientes()
 
     
 
     //get one Cliente
-    $scope.loadCliente = function (clienteId) {
-        
+    $scope.findClienteById = function (clienteId) {
         clienteService.findOneById(clienteId).then(function (response) {
             $scope.cliente = response.data
             console.log(response.data)
-            
         }).catch(function (error) {
             //alert(error.data.message)
         })
-    }
-
-    //get Cliente Id
-    $scope.getClienteId = function (idString) {
-        let id = parseInt(idString)
-        console.log(id)
-        return id
     }
 
     //put Cliente
     $scope.putCliente = function (cliente) {
         clienteService.putCliente(cliente).then(function (response) {
             console.log(response.data)
-            this.loadClientes()
+            this.findClientes()
         }).catch(function (error) {
-            alert(error.data.message)
+            //alert(error.data.message)
         })
     }
 
     //post one
     $scope.postCliente = function (cliente) {
         clienteService.postCliente(cliente).then(function (response) {
-            this.loadClientes()
-            $scope.showCli()
+            this.findClientes()
             console.log(response)
         }).catch(function (error) {
             alert(error.data.message)
@@ -61,7 +52,7 @@ angular.module("ProjetoBethaFrontEnd").controller("clienteController", function 
     $scope.deleteCliente = function (clienteId) {
         clienteService.deleteCliente(clienteId).then(function (response) {
             console.log(response.data)
-            this.loadClientes()
+            this.findClientes()
         }).catch(function (error) {
             alert(error.data.message)
         })
